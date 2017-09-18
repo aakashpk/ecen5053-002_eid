f030e4e048aef706e4d3e8b755168c9f6a1eef57
Aakash <Aakash.Kumar@colorado.edu>
2017-09-18 06:09:23 +0000

    dht_read working with F/C selection from command line parameter

--- dht_read.py
+++ dht_read.py
@@ -1,19 +1,21 @@
 #DHT22 library from Adafruit
 import Adafruit_DHT
+import sys
 
 sensor = Adafruit_DHT.DHT22
 pin = 4
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 temp_f=((temperature*1.8)+32)
 
-if(sys.argv[1]==f)
-	temp=temp_f
+
+if(sys.argv[1]=='f'):
+	temperature=temp_f
 
 if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temp, humidity))
+   print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
