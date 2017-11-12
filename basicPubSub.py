6107aa4b731f3eeddf8104656c0db0cb8abf767d
Aakash <Aakash.Kumar@colorado.edu>
2017-11-12 13:54:26 -0700

    Changed the way temperature data is read. added in lambda code.

--- basicPubSub.py
+++ basicPubSub.py
@@ -45,14 +45,17 @@ parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket
 parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub", help="Targeted client id")
 parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
 
-args = parser.parse_args()
-host = args.host
-rootCAPath = args.rootCAPath
-certificatePath = args.certificatePath
-privateKeyPath = args.privateKeyPath
-useWebsocket = args.useWebsocket
-clientId = args.clientId
-topic = args.topic
+basicPubSub.py -e a3c1qeo00yd0b1.iot.us-west-2.amazonaws.com -r root-CA.crt -c raspberry-pi.cert.pem -k raspberry-pi.private.key
+
+#args = parser.parse_args()
+host = 'a3c1qeo00yd0b1.iot.us-west-2.amazonaws.com' #args.host
+rootCAPath = 'root-CA.crt' #args.rootCAPath
+certificatePath = 'raspberry-pi.cert.pem' #args.certificatePath
+privateKeyPath = 'raspberry-pi.private.key' #args.privateKeyPath
+useWebsocket = False #args.useWebsocket
+clientId = basicPubSub #args.clientId
+#topic = args.topic
+topic='proj3/rpi'
 
 if args.useWebsocket and args.certificatePath and args.privateKeyPath:
 	parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
@@ -93,10 +96,11 @@ myAWSIoTMQTTClient.connect()
 myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
 time.sleep(2)
 
-topic='proj3/rpi'
 
-# send temperature and humidity
+
+
 def publishTempHumidity():
+"""" Function to publish temp and humidity data to AWS using MQTT"""
     global dbSamplingInterval,topic
     threading.Timer(dbSamplingInterval,publishTempHumidity).start() # to autorun function once every update interval
     ts=datetime.now()
@@ -105,7 +109,7 @@ def publishTempHumidity():
     msg = '{'+msg+'}'
     myAWSIoTMQTTClient.publish(topic, msg, 1)
 
-publishTempHumidity()
+publishTempHumidity() # send temperature and humidity
 
 '''
 # Publish to the same topic in a loop forever
