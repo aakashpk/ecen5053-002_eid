7ac078d322b3657cfe9d3543b166f20485bc832b
Aakash <Aakash.Kumar@colorado.edu>
2017-11-12 14:15:37 -0700

    change to sensor read

--- sensorRead.py
+++ sensorRead.py
@@ -6,12 +6,20 @@ Created on Wed Oct 18 17:30:24 2017
 """
 
 import Adafruit_DHT
+import threading
+from datetime import datetime
 
 sensor = Adafruit_DHT.DHT22
 pin = 4
 
+def getData():
+    threading.Timer(2,getData).start()
+    temperature, humidity=Adafruit_DHT.read_retry(sensor, pin)
+    print("data read at ",datetime.now(),": T is :",temperature,"H is: ",humidity)
+
 def get_TempHum():
-    return Adafruit_DHT.read_retry(sensor, pin)
+    global temperature, humidity
+    return temperature, humidity
 
 def todegF(degc):
     return (degc*1.8)+32
