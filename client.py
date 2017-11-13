84a38685c17d1c7d32e2650cae249d239d435304
Hari93 <hama5569@colorado.edu>
2017-11-12 20:15:01 -0700

    Client code with QT updated

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-!/usr/bin/python
+#!/usr/bin/python
 # Author : Hari Shreenivash
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
